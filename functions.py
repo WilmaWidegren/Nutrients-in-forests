1e5ce89dd4eeabd7a0f2845ef1ec74d1a587fac2
chalmersgustav <jarlstam@chalmers.se>
2025-12-09 15:54:29 +0100

    Ändrat massor i functions :((

--- functions.py
+++ functions.py
@@ -12,18 +12,18 @@ def get_tree_positions(Number_of_trees, size_of_forest):
     return x_positions, y_positions
 
 
-def grow_initial_forest(Number_of_trees , max_age):
-    tree_ages = []
+def grow_initial_forest(Number_of_trees , max_size):
+    tree_size = []
     for i in range(Number_of_trees):
-        tree_ages.append(np.random.uniform(1, max_age))
-    return np.array(tree_ages)
+        tree_size.append(np.random.uniform(1, max_size))
+    return np.array(tree_size)
     
 
-def calculate_tree_growth(x :float, k: float, a: float, A: float, m: float, C: float) -> np.ndarray:
+def calculate_tree_growth(x :float, k: float, A: float, m: float, C: float) -> np.ndarray:
     """
     Calculates tree growth (Y) based on a modified sigmoidal growth function.
 
-    The formula is: Y = k * (1 - e^((a + b * S) * A))^m
+    The formula is: Y = k * (1 - e^(x * A * C))^m
 
     This function is designed to model growth where juvenile trees grow faster 
     than mature ones, characterized by an S-shaped (sigmoidal) curve.
@@ -31,21 +31,20 @@ def calculate_tree_growth(x :float, k: float, a: float, A: float, m: float, C: f
     Parameters:
     - x (np.ndarray): Age/Time of the tree. Randomly generated when initialising the forest.
     - k (float): Asymptotic scaling factor; controls the maximum potential size.
-    - a (float): Initial coefficient influencing the growth rate. Coal?
     - A (float): Rate parameter; influences the speed at which the maximum size is approached.
     - m (float): Shape parameter; determines the point of inflection (when acceleration peaks).
     - C (float): Limiting factor; carbon reservior. Avaliable / Needed. If C = 1 -> Optimal growth.
 
     Returns:
-    - np.ndarray: Calculated tree growth (Y) for each age (x).
+    - np.ndarray: Calculated tree growth (Y) for each size (x).
     """
     
     # Ensure all inputs are float for calculation
-    k, a, A, m = float(k), float(a), float(A), float(m)
+    k, A, m = float(k), float(A), float(m)
     
     # Calculate the exponent term
-    # Exponent = ((a + x) * A)
-    exponent_term = (a + x) * A * C
+    # Exponent = (x * A * C)
+    exponent_term = x * A * C
     
     # Calculate the core sigmoid term
     # Sigmoid = (1 - e^(Exponent))^m
@@ -115,27 +114,26 @@ def uppdate_carbon_matrix(tree_size: np.ndarray, conection_matrix: np.ndarray, c
 
     return carbon_list
 
-def no_mycorrhiza(NUM_TREES, T, initial_forest_age):
+def no_mycorrhiza(NUM_TREES, T, initial_forest_size):
     ### Constants ###
     FACTOR = np.random.uniform(0.03, 0.06)
     A = -0.015
     dt = 1
-    a = 0.01
     k = 40
     m = 1.2
     c = 1
     COST_FOR_GROWTH = 1.3
 
     ### Initialisation ###
-    forest_size = calculate_tree_growth(initial_forest_age, k, a, A, m, c)
+    forest_size = initial_forest_size.copy() # calculate_tree_growth(initial_forest_size, k, A, m, c)
     time = 0
-    carbon = calculate_max_carbon(forest_size, FACTOR)
+    carbon = carbon = np.ones_like(forest_size) # calculate_max_carbon(forest_size, FACTOR)
     plot_data = [[] for _ in range(NUM_TREES)] # Create lists to plot the tree growth
     plot_average = []
-    forest_age = initial_forest_age
+    forest_age = initial_forest_size
     ### Main ###
     while time < T:
-        next_size = calculate_tree_growth(forest_age + dt, k, a, A, m, carbon)
+        next_size = calculate_tree_growth(forest_size, k, A, m, carbon)
         growth_increment = next_size - forest_size # Calculates how much the tree grows for each timestep.
 
         for i in range(NUM_TREES):
@@ -160,27 +158,26 @@ def no_mycorrhiza(NUM_TREES, T, initial_forest_age):
 
     return plot_data, plot_average
 
-def mycorrhiza(NUM_TREES, LATTICE_SIZE, T, initial_forest_age):
+def mycorrhiza(NUM_TREES, LATTICE_SIZE, T, initial_forest_size):
     ### Constants ###
     FACTOR = np.random.uniform(0.03, 0.06)
-    COST_FOR_GROWTH = 1
+    COST_FOR_GROWTH = 1.3
     A = -0.015
     dt = 1
-    a = 0.01
     k = 40
     m = 1.2
     c = 1
 
     ### Initialisation ###
-    forest_size = calculate_tree_growth(initial_forest_age, k, a, A, m, c)
+    forest_size = initial_forest_size.copy() # calculate_tree_growth(initial_forest_size, k, A, m, c)
     time = 0
-    carbon = calculate_max_carbon(forest_size, FACTOR)
+    carbon = np.ones_like(forest_size) # calculate_max_carbon(forest_size, FACTOR)
     plot_data = [[] for _ in range(NUM_TREES)] # Create lists to plot the tree growth
     plot_average = []
 
     x, y = get_tree_positions(NUM_TREES, LATTICE_SIZE)
     conection_matrix = get_conections(x, y)
-    forest_age = initial_forest_age
+    forest_age = initial_forest_size
 
     ### Main ###
     while time < T:
@@ -188,7 +185,7 @@ def mycorrhiza(NUM_TREES, LATTICE_SIZE, T, initial_forest_age):
             new_carbon = uppdate_carbon_matrix(forest_size, conection_matrix, carbon)
             carbon=new_carbon
 
-        next_size = calculate_tree_growth(forest_age + dt, k, a, A, m, carbon)
+        next_size = calculate_tree_growth(forest_size, k, A, m, carbon)
         growth_increment = next_size - forest_size # Calculates how much the tree grows for each timestep.
         
         for i in range(NUM_TREES):
--- main.py
+++ main.py
@@ -3,27 +3,30 @@ import matplotlib.pyplot as plt
 
 ### CONSTANTS ###
 NUM_TREES = 300
-T = 80
+T = 100
 LATTICE_SIZE = 20
 
 ### INITIALISATION ###
-initial_forest_age = f.grow_initial_forest(NUM_TREES, 40)
-initial_forest_age_no = initial_forest_age.copy()
-initial_forest_age_myc = initial_forest_age.copy()
+initial_forest_size = f.grow_initial_forest(NUM_TREES, 30)
+initial_forest_size_no = initial_forest_size.copy()
+initial_forest_size_myc = initial_forest_size.copy()
+for i in range(10):
+    initial_forest_size_no = initial_forest_size.copy()
+    initial_forest_size_myc = initial_forest_size.copy()
+    ### NO MYCORRHIZA ###
+    plot_data_no, plot_average_no = f.no_mycorrhiza(NUM_TREES, T, initial_forest_size_no)
 
-### NO MYCORRHIZA ###
-plot_data_no, plot_average_no = f.no_mycorrhiza(NUM_TREES, T, initial_forest_age_no)
+    ### MYCORRHIZA ###
+    plot_data_myc, plot_average_myc = f.mycorrhiza(NUM_TREES, LATTICE_SIZE, T, initial_forest_size_myc)
 
-### MYCORRHIZA ###
-plot_data_myc, plot_average_myc = f.mycorrhiza(NUM_TREES, LATTICE_SIZE, T, initial_forest_age_myc)
+    ### Plot ###
+    f.plot_graph(NUM_TREES, T, plot_data_no, plot_average_no) # Plot of no mycorrhiza
 
-### Plot ###
-f.plot_graph(NUM_TREES, T, plot_data_no, plot_average_no) # Plot of no mycorrhiza
+    f.plot_network(NUM_TREES, LATTICE_SIZE) # Example of a network. Shows the connections for one tree
 
-f.plot_network(NUM_TREES, LATTICE_SIZE) # Example of a network. Shows the connections for one tree
+    f.plot_graph(NUM_TREES, T, plot_data_myc, plot_average_myc) # Plot of mycorrhiza
 
-f.plot_graph(NUM_TREES, T, plot_data_myc, plot_average_myc) # Plot of mycorrhiza
-
-plt.plot(plot_average_no)
-plt.plot(plot_average_myc)
-plt.show()
+    plt.plot(plot_average_no, label = "no")
+    plt.plot(plot_average_myc)
+    plt.legend()
+    plt.show()
